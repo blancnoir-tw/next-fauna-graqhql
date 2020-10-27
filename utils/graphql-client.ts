0e264c16402686b3cf2a8b2302f35ddcd1c86041
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-10-28 06:58:13 +0900

    update graphql-clients

--- utils/graphql-client.ts
+++ utils/graphql-client.ts
@@ -2,9 +2,13 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = 'https://graphql.fauna.com/graphql'
 
-export const graphQLClient = new GraphQLClient(endpoint, {
-  headers: {
-    authorization: `Bearer ${process.env.NEXT_PUBLIC_FAUNA_SECRET}`,
-    'X-Schema-Preview': 'partial-update-mutation',
-  },
-})
+export const graphQLClient = (token?: string | null) => {
+  const secret = token || process.env.NEXT_PUBLIC_FAUNA_GUEST_SECRET
+
+  return new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${secret}`,
+      'X-Schema-Preview': 'partial-update-mutation',
+    },
+  })
+}
